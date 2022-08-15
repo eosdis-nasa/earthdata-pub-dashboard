265cbee9a727e1b023d9590e71eae9648abb3386
Kimberly Broughton <broughtonkk@ornl.gov>
2022-08-15 12:35:59 -0400

    removed unused lib

--- app/src/js/components/Requests/approval.js
+++ app/src/js/components/Requests/approval.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { withRouter, Link } from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
 import {
   getRequest,
   reviewRequest
