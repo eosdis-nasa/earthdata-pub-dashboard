b16669f951c5cc515cb10b6f91a7dd6dc2814624
Davis Gaunt <bdg0015@uah.edu>
2023-06-20 13:59:02 -0500

    integration clean up

--- app/src/js/components/DataUpload/overview.js
+++ app/src/js/components/DataUpload/overview.js
@@ -1,16 +1,11 @@
 'use strict';
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { withRouter } from 'react-router-dom';
 import { connect, useDispatch } from 'react-redux';
 import _config from '../../config';
-import {
-  getPutUrl
-} from '../../actions';
 import { loadToken } from '../../utils/auth';
-import { strings } from '../locale';
 import Breadcrumbs from '../Breadcrumbs/Breadcrumbs';
-import { createMD5 } from 'hash-wasm';
 import localUpload from 'edpub-data-upload-utility';
 
 const breadcrumbConfig = [
@@ -28,76 +23,17 @@ const {apiRoot} = _config;
 
 const UploadOverview = ({ signedPut }) => {
   const [statusMsg, setStatusMsg] = useState('Select a file');
-  const [uploadFile, setUploadFile] = useState('');
-  const [fileHash, setFileHash] = useState('');
   let hiddenFileInput = React.createRef(null);
-  const chunkSize = 64 * 1024 * 1024;
-  const fileReader = new FileReader();
-  let hasher = null;
 
-  const dispatch = useDispatch();
-
-  const put = async (url) => {
-    const resp = await fetch(url, {
-      method: 'PUT',
-      headers: {
-        'Content-Type': uploadFile.type
-      },
-      body: uploadFile
-    }).then((resp) => {
-      if (resp.status !== 200) {
-        setStatusMsg('Select a file');
-      } else {
-        setStatusMsg('Upload Complete');
-        setTimeout(() => {
-          setStatusMsg('Select another file');
-          hiddenFileInput = React.createRef(null);
-        }, '5000');
-      }
-    });
-    return resp;
-  };
-
-  const hashChunk = (chunk) => {
-    return new Promise((resolve, reject) => {
-      fileReader.onload = async (e) => {
-        const view = new Uint8Array(e.target.result);
-        hasher.update(view);
-        resolve();
-      };
-
-      fileReader.readAsArrayBuffer(chunk);
-    });
-  };
-
-  const readFile = async (file) => {
-    if (hasher) {
-      hasher.init();
-    } else {
-      hasher = await createMD5();
-    }
-
-    const chunkNumber = Math.floor(file.size / chunkSize);
-
-    for (let i = 0; i <= chunkNumber; i++) {
-      const chunk = file.slice(
-        chunkSize * i,
-        Math.min(chunkSize * (i + 1), file.size)
-      );
-      await hashChunk(chunk);
-    }
-    const hash = hasher.digest();
-    return Promise.resolve(hash);
-  };
+  // const dispatch = useDispatch();
 
   const handleClick = event => {
     hiddenFileInput.current.click();
   };
 
   const handleChange = async event => {
-    setStatusMsg('Preparing for Upload');
+    setStatusMsg('Uploading...');
     const file = event.target.files[0];
-    setUploadFile(file);
     const upload = new localUpload();
     console.log(loadToken().token)
     const resp = await upload.uploadFile({
@@ -105,23 +41,10 @@ const UploadOverview = ({ signedPut }) => {
       apiEndpoint: `${apiRoot}data/upload/getPostUrl`, 
       authToken: loadToken().token
     });
-    console.log(resp);
-    // const hash = await readFile(file);
-    // setFileHash(hash);
-    // const payload = {
-    //   file_name: file.name,
-    //   file_type: file.type,
-    //   checksum_value: hash
-    // };
-    // setStatusMsg('Uploading');
-    // dispatch(getPutUrl(payload));
+    if(resp === 'Upload successfull'){
+      setStatusMsg('Upload Complete');
+    }
   };
-
-  // useEffect(async () => {
-  //   if (signedPut !== {}) {
-  //     await put(signedPut.url);
-  //   } 
-  // }, [signedPut]);
 
   return (
     <div className='page__component'>
