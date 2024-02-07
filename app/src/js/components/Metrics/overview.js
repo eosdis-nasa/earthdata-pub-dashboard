5d726fd7eee0e4d6c4f31cbf0de2f558797233e7
Kimberly Broughton <broughtonkk@ornl.gov>
2024-02-07 15:47:26 -0500

    added metrics

--- app/src/js/components/Metrics/overview.js
+++ app/src/js/components/Metrics/overview.js
@@ -1,60 +1,116 @@
 'use strict';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
-import { withRouter } from 'react-router-dom';
-import { connect, useDispatch } from 'react-redux';
+import { withRouter, useHistory } from 'react-router-dom';
+import { connect } from 'react-redux';
+import Loading from '../LoadingIndicator/loading-indicator';
+import LoadingEllipsis from '../LoadingEllipsis/loading-ellipsis';
+import SearchModal from '../SearchModal';
 import { lastUpdated } from '../../utils/format';
 
 import {
-  listMetrics,
-  // getCloudMetric
-  // searchMetrics,
-  // clearMetricsSearch
+  listCloudMetrics,
+  listDaacs,
+  listWorkflows,
+  // getRequestDetails,
+  // searchRequests,
+  // clearRequestsSearch,
 } from '../../actions';
 import List from '../Table/Table';
-// import Search from '../Search/search';
-import { tableColumns } from '../../utils/table-config/metrics';
-// import ListFilters from '../ListActions/ListFilters';
+import Select from 'react-select';
+import { metricOptionNames, workflowOptionNames, stateOptionNames, daacOptionNames } from '../../selectors';
+import Search from '../Search/search';
+import { tableColumns, requestTableColumns, timeColumns, countColumns } from '../../utils/table-config/metrics';
 import { strings } from '../locale';
 import Breadcrumbs from '../Breadcrumbs/Breadcrumbs';
 
-const breadcrumbConfig = [
-  {
-    label: 'Dashboard Home',
-    href: '/'
-  },
-  {
-    label: 'Metrics',
-    active: true
-  }
-];
-
-const MetricOverview = ({ metrics }) => {
-  const dispatch = useDispatch();
+const MetricOverview = ({ dispatch, match, daacs, workflows, requests, metrics, states }) => {
+  const { requestId } = match.params.requestId ? match.params.requestId : '';
+  const metric = match.params.metric ? match.params.metric : undefined;
   useEffect(() => {
-    dispatch(listMetrics({ count: true }));
-  }, [metrics.searchString, dispatch]);
-  const { queriedAt } = metrics.list.meta;
-  // Below cannot be fully tested until metrics reports are live
-  /* dispatch(getCloudMetric('2021-06-20')).then(
-    function (image) {
-      const byteCharacters = atob(image.data);
-      const byteArrays = [];
-      for (let offset = 0; offset < byteCharacters.length; offset += 1024) {
-        const slice = byteCharacters.slice(offset, offset + 1024);
-        const byteNumbers = new Array(slice.length);
-        for (let i = 0; i < slice.length; i++) {
-          byteNumbers[i] = slice.charCodeAt(i);
-        }
-        const byteArray = new Uint8Array(byteNumbers);
-        byteArrays.push(byteArray);
-      }
-      const blob = new Blob(byteArrays, {
-        type: 'image/jpeg'
-      });
-      document.getElementById('testimage').src = URL.createObjectURL(blob);
+    dispatch(listDaacs());
+    dispatch(listWorkflows());
+    // dispatch(getRequestDetails(requestId));
+    dispatch(listCloudMetrics(metric));
+  }, []);
+
+  useEffect(() => {
+    setDaacOptions(daacs.map(({ id, short_name }) => ({ value: id, label: short_name })));
+    setWorkflowOptions(workflows.map(({ id, long_name }) => ({ value: id, label: long_name })));
+    // requests.searchString,
+    setMetricOptions(metricOptionNames);
+    setStateOptions(stateOptionNames);
+  }, [daacs, workflows, requests, metrics, states]);
+
+  const history = useHistory();
+  // const [showSearch, setShowSearch] = useState(false);
+  // const [searchOptions, setSearchOptions] = useState({});
+  const [daacOptions, setDaacOptions] = useState([]);
+  const [selectedDaacs, setSelectedDaacs] = useState([]);
+  const [workflowOptions, setWorkflowOptions] = useState([]);
+  const [selectedWorkflows, setSelectedWorkflows] = useState([]);
+  // const [request, setRequest] = useState('');
+  // const [validRequest, setValidRequest] = useState(true);
+  const [metricOptions, setMetricOptions] = useState([]);
+  const [selectedMetrics, setSelectedMetrics] = useState([]);
+  const [stateOptions, setStateOptions] = useState([]);
+  const [selectedStates, setSelectedStates] = useState([]);
+
+  useEffect(() => {
+    handleSubmit()
+  }, [selectedDaacs, selectedMetrics, selectedWorkflows, selectedStates])
+
+  const breadcrumbConfig = [
+    {
+      label: 'Dashboard Home',
+      href: '/'
+    },
+    {
+      label: 'Metrics',
+      active: true
     }
-  ); */
+  ];
+
+  /* const extractId = (list) => {
+    const rtnList = [];
+    list.forEach(item => {
+      rtnList.push(item.value);
+    });
+    return rtnList;
+  }; */
+
+  const handleDaacSelect = (data) => {
+    setSelectedDaacs(data);
+  };
+
+  const handleWorkflowSelect = (data) => {
+    setSelectedWorkflows(data);
+  };
+
+  const handleRequest = (value) => {
+    setRequest(value);
+  };
+
+  const handleMetricSelect = (data) => {
+    setSelectedMetrics(data);
+  };
+
+  const handleStateSelect = (data) => {
+    setSelectedStates(data);
+  };
+
+  const handleSubmit = async () => {
+    const payload = {
+      daac_id: selectedDaacs?.value,
+      workflow_id: selectedWorkflows?.value,
+      metric: selectedMetrics?.value,
+      state: selectedStates?.value
+    };
+    history.push('/metrics');
+    await dispatch(listCloudMetrics(payload))
+  };
+  
+  
   return (
     <div className='page__component'>
       <section className='page__section page__section__controls'>
@@ -63,35 +119,87 @@ const MetricOverview = ({ metrics }) => {
       <section className='page__section page__section__header-wrapper'>
         <div className='page__section__header'>
           <h1 className='heading--large heading--shared-content with-description '>{strings.metric_overview}</h1>
-          {lastUpdated(queriedAt)}
         </div>
       </section>
+      {/* {showSearch && <SearchModal {...searchOptions} />} */}
       <section className='page__section'>
         <div className='heading__wrapper--border'>
-          <h2 className='heading--medium heading--shared-content with-description'>{strings.all_metrics} <span className='num--title'>{metrics.list.data.length}</span></h2>
+          <h2 className='heading--medium heading--shared-content with-description'>{strings.all_metrics} <span className='num--title'>{metrics?.list?.data?.length}</span></h2>
         </div>
+        {metrics?.list?.data?.length > 0 ?
         <List
-          list={metrics.list}
+          list={metrics?.list}
           dispatch={dispatch}
-          action={listMetrics}
-          tableColumns={tableColumns}
+          action={listCloudMetrics}
+            tableColumns={selectedMetrics?.value && selectedMetrics?.value.match(/time_to_publish/g) ? timeColumns : selectedMetrics?.value && selectedMetrics?.value.match(/user_count/g) ? countColumns : requestTableColumns}
           query={{}}
           bulkActions={[]}
-          rowId='name'
+          rowId='event'
         >
-          {/* <ListFilters>
-            <Search
+            <div className="filterGrid">
+            {/* <Search
               dispatch={dispatch}
-              action={searchMetrics}
-              clear={clearMetricsSearch}
+              action={handleRequest}
+              clear={clearRequestsSearch}
               label='Search'
-              placeholder="Metric Name"
-            />
-          </ListFilters> */}
+              placeholder="Request ID"
+            
+  /> */}    
+            <label className='heading--small'>State
+              <Select
+                id="stateSelect"
+                options={stateOptions}
+                value={selectedStates}
+                onChange={handleStateSelect}
+                isSearchable={true}
+                isMulti={false}
+                isClearable={true}
+                className='selectButton'
+                placeholder='Select State ...'
+                aria-label='Select State'
+              /></label>
+              <label className='heading--small'>Workflows
+                <Select
+                  id="workflowSelect"
+                  options={workflowOptions}
+                  value={selectedWorkflows}
+                  onChange={handleWorkflowSelect}
+                  isSearchable={true}
+                  isMulti={false}
+                  isClearable={true}
+                  className='selectButton'
+                  placeholder='Select Workflows ...'
+                  aria-label='Select Workflows'
+                /></label>
+            <label className='heading--small'>Daacs
+              <Select
+                id="daacSelect"
+                options={daacOptions}
+                value={selectedDaacs}
+                onChange={handleDaacSelect}
+                isSearchable={true}
+                isMulti={false}
+                isClearable={true}
+                className='selectButton'
+                placeholder='Select Daac ...'
+                aria-label='Select Daac'
+              /></label>
+              <label className='heading--small'>Metrics
+                <Select
+                  id="metricSelect"
+                  options={metricOptions}
+                  value={selectedMetrics}
+                  onChange={handleMetricSelect}
+                  isSearchable={true}
+                  isMulti={false}
+                  isClearable={true}
+                  className='selectButton'
+                  placeholder='Select Metrics ...'
+                  aria-label='Select Metrics'
+                /></label>
+            </div>
         </List>
-        {/* <div>
-          <img id="testimage" alt="metrics-test-image"/>
-        </div> */}
+          : <Loading />}
       </section>
     </div>
   );
@@ -99,9 +207,31 @@ const MetricOverview = ({ metrics }) => {
 
 MetricOverview.propTypes = {
   dispatch: PropTypes.func,
-  metrics: PropTypes.object
+  metrics: PropTypes.object,
+  privileges: PropTypes.object,
+  match: PropTypes.object,
+  user_groups: PropTypes.array,
+  groups: PropTypes.array,
+  daacs: PropTypes.array,
+  states: PropTypes.array,
+  workflows: PropTypes.array,
+  requests: PropTypes.object,
+  workflowOptions: PropTypes.array,
+  stateOptions: PropTypes.array,
+  metricOptions: PropTypes.array,
+  daacOptions: PropTypes.array,
 };
 
-export default withRouter(connect(
-  (state) => ({ metrics: state.metrics })
-)(MetricOverview));
+export default withRouter(connect(state => ({
+  metrics: state.metrics,
+  user_groups: state.api.tokens.groups,
+  groups: state.groups.list.data,
+  daacs: state.daacs.list.data,
+  workflows: state.workflows.list.data,
+  requests: state.requests,
+  states: state.states,
+  workflowOptions: workflowOptionNames(state),
+  stateOptions: stateOptionNames(state),
+  metricOptions: metricOptionNames(state),
+  daacOptions: daacOptionNames(state),
+}))(MetricOverview));
