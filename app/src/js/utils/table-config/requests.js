8ad529a552ca2f029dd4bd094b7bd44d03899470
deepakbacharya <deepak.acharya@uah.edu>
2024-08-02 10:47:45 -0500

    testurl

--- app/src/js/utils/table-config/requests.js
+++ app/src/js/utils/table-config/requests.js
@@ -152,14 +152,14 @@ export const stepLookup = (row) => {
             // Build url to forms app - after submitted
             if (tmpType.match(/form/g)) {
               console.log('${_config.formsUrl} 1', _config.formsUrl)
-              request = `/form/questions/${row.id}`;
+              request = `${_config.formsUrl}/form/questions/${row.id}`;
             }
           }
         }
         // Build url to forms app if not submitted
         if (stepType.match(/form/g)) {
           console.log('${_config.formsUrl}', _config.formsUrl)
-          request = `/form/questions/${row.id}`;
+          request = `${_config.formsUrl}/form/questions/${row.id}`;
         // assign a workflow
         } else if (stepType.match(/action/g)) {
           request = `/workflows?requestId=${row.id}`;
