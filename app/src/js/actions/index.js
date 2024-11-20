6432c65a36a82fa10f81515256688dd2716fe656
camposeddie <eddie.campos@uah.edu>
2024-11-20 13:58:46 -0600

    Increase timeout of reply in attempt to include attachment in refresh

--- app/src/js/actions/index.js
+++ app/src/js/actions/index.js
@@ -999,7 +999,7 @@ export const replyConversation = (payload) => {
       .then(() => {
         setTimeout(() => {
           dispatch(getConversation(payload.conversation_id));
-        }, 1000);
+        }, 2000);
       });
   };
 };
