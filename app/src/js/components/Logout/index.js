2d8d55c7047880c478fb73ce371646f437bcb80c
camposeddie <eddie.campos@uah.edu>
2025-09-05 16:59:14 -0400

    Standardize logout component naming

--- app/src/js/components/Logout/index.js
+++ app/src/js/components/Logout/index.js
@@ -7,8 +7,8 @@ const { basepath } = _config;
 export const Logout = () => {
     const clearCacheAndRedirect = () => {
         deleteToken();
-        window.location.href = `${basepath}auth`
+        window.location.href = `${basepath}auth`;
         return null;
     }
     return clearCacheAndRedirect();
-}
+};
